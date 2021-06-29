dd9ed2bea2a467178aae29d60d735d0d2666413f
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-06-28 22:03:26 -0300

    Initial scaffolding tests

---
+++ ExemploApiCatalogoJogosTestes/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ExemploApiCatalogoJogosTestes
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ExemploApiCatalogoJogosTestes/UnitTests/JogosServiceTest.cs
@@ -0,0 +1,7 @@
+namespace ExemploApiCatalogoJogosTestes.UnitTests
+{
+    public class JogosServiceTest
+    {
+        
+    }
+}
